1a441deb1082eaf2dae39f42b7a4fbccc9ac6252
DESKTOP-3010E34\Haohao <haohaostudio-e@foxmail.com>
2019-04-14 20:23:27 +0800

    Ver1.0

---
+++ AngryDick/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1555244265,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/angrybackground.png",
+		"images/buttons-sheet0.png",
+		"images/buttons-sheet1.png",
+		"images/bgs-sheet0.png",
+		"images/bgs-sheet1.png",
+		"images/blocks-sheet0.png",
+		"images/bowwheel-sheet0.png",
+		"images/bow-sheet0.png",
+		"images/wood-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"html2canvas.min.js",
+		"animate.css",
+		"hmmg_layoutTransition.css"
+	]
+}
